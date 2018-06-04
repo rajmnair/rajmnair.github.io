c25211f26e06c1ac8d2571d8962fe1f3cf9b494a
Dhruv Saidava <dhruvsaidava@gmail.com>
2018-06-04 10:00:14 +0530

    Temp Check website before purchase

--- web/js/erika.google.map.js
+++ web/js/erika.google.map.js
@@ -9,9 +9,8 @@ function init() {
         zoom: 11,
 
         scrollwheel: false,
-
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(3.1575706, 101.6887932), // New York
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
