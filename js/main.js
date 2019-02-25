1bd3e62e8c24a7aa52971ba7efac2411378e9765
Fotariya Jimish <fotariyajimish@gmail.com>
2019-02-25 14:30:52 +0530

    analytics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-92728001-2');
+
+
 (function($) {
     'use strict';
 
