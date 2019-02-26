f240a049041b4102d9a6cebb12bdff133eb652d1
Fotariya Jimish <fotariyajimish@gmail.com>
2019-02-26 15:03:53 +0530

    slider in z2m

---
+++ app.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+              $('.single-project-slider').slick({
+                    dots: true,
+                    infinite: true,
+                    speed: 300,
+                    slidesToShow: 3,
+                    adaptiveHeight: false,
+                    arrows: true,
+                });
+
+})
